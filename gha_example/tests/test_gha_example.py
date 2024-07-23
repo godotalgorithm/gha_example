f9b6a2bab603190b5ccf7fba811f7f753be9acef
Jonathan Moussa <jemoussa@vt.edu>
2024-07-23 16:59:15 -0400

    Remove hard-to-cover code

--- gha_example/tests/test_gha_example.py
+++ gha_example/tests/test_gha_example.py
@@ -5,11 +5,10 @@ Unit and regression test for the gha_example package.
 # Import package, test suite, and other packages as needed
 import sys
 
+import mock
 import pytest
 
 import gha_example
-
-import subprocess
 
 def test_gha_example_imported():
     """Sample test, will always pass so long as import statement worked."""
@@ -18,7 +17,3 @@ def test_gha_example_imported():
 def test_canvas():
     """A simple test that can conditionally fail"""
     assert "Henry David Thoreau" in gha_example.canvas()
-
-def test_main():
-    """Cover the command-line use of the Python script"""
-    subprocess.run(["python", "../gha_example.py"])    
